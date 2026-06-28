9b8dce6850e9946803632bfd60d2491613a1caeb
Greenl1s <136644511+Greenl1s@users.noreply.github.com>
2026-06-28 23:47:50 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,10 +3,17 @@ export const CONFIG = {
   publicKey: 'https://disk.yandex.ru/i/kAXYp4XPyNJAWg',
   writeSecret: 'mySecretKey123'
 };
-export const SHEETS = { instruments: 'Приборы', history: 'История', users: 'Пользователи', retired: 'Списанные' };
+
+export const SHEETS = {
+  instruments: 'Приборы',
+  history: 'История',
+  users: 'Пользователи',
+  retired: 'Списанные'
+};
+
 export const HEADERS = {
-  instruments: ['id','name','serial_number','model','type','verification_date','valid_until','document_url','condition','taken_by','taken_where','taken_date'],
-  history: ['instrument_id','instrument_name','user','place','issue_date','return_date','returned_by','operation_date'],
+  instruments: ['id','name','serial_number','model','type','verification_date','valid_until','document_url','condition','taken_by','taken_where','taken_extra','taken_date'],
+  history: ['instrument_id','instrument_name','user','place','extra_data','issue_date','return_date','returned_by','operation_date'],
   users: ['username','password','role'],
-  retired: ['id','name','serial_number','model','type','verification_date','valid_until','document_url','condition','taken_by','taken_where','taken_date','retired_date']
+  retired: ['id','name','serial_number','model','type','verification_date','valid_until','document_url','condition','taken_by','taken_where','taken_extra','taken_date','retired_date']
 };
