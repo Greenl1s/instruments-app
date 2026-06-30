a9f57327562c4fe1abd9abf21f1b1397ed2cbf22
Greenl1s <136644511+Greenl1s@users.noreply.github.com>
2026-06-30 14:39:14 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   proxyUrl: 'https://prox-cstroy.vercel.app',
-  publicKey: 'https://disk.yandex.ru/i/ciFZRWIX5aqjNg',
+  publicKey: 'https://disk.yandex.ru/i/i4bNYfeA6LZSAQ',
   writeSecret: 'mySecretKey123',
   filePath: '/Учёт.xlsx'
 };
