30fb78f38d8c948d323169f97ade12747ec36c12
Greenl1s <136644511+Greenl1s@users.noreply.github.com>
2026-07-02 14:39:31 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
-  proxyUrl: 'https://prox-cstroy.vercel.app',
+  proxyUrl: 'https://proxy-cstroy.vercel.app',  // ваш URL
   publicKey: 'https://disk.yandex.ru/i/jU1KxLsivSfagA',
-  writeSecret: 'mySecretKey123',
+  writeSecret: 'mySecretKey123'
 };
 
 export const SHEETS = {
@@ -12,7 +12,7 @@ export const SHEETS = {
 };
 
 export const HEADERS = {
-  instruments: ['id','name','serial_number','model','type','verification_date','valid_until','document_url','condition','taken_by','taken_where','taken_extra','taken_date','booked_by','booked_date'],
+  instruments: ['id','name','serial_number','model','type','verification_date','valid_until','document_url','condition','taken_by','taken_where','taken_extra','taken_date','booked_by','booked_date','booked_extra'],
   history: ['instrument_id','instrument_name','user','place','extra_data','issue_date','return_date','returned_by','operation_date'],
   users: ['username','password','role','extra'],
   retired: ['id','name','serial_number','model','type','verification_date','valid_until','document_url','condition','taken_by','taken_where','taken_extra','taken_date','retired_date']
