66294f3e9a55ec9934fa49fefb8446da2f26ae36
Greenl1s <136644511+Greenl1s@users.noreply.github.com>
2026-07-03 16:06:46 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -16,7 +16,7 @@ export const SHEETS = {
 };
 
 export const HEADERS = {
-  instruments: ['id','name','serial_number','model','type','verification_date','valid_until','document_url','condition','taken_by','taken_where','taken_extra','taken_date','booked_by','booked_date','booked_extra','photo_url'],
+  instruments: ['id','name','serial_number','model','type','verification_date','valid_until','document_url','condition','taken_by','taken_where','taken_extra','taken_date','booked_by','booked_date','booked_extra'],
   history: ['instrument_id','instrument_name','user','place','extra_data','issue_date','return_date','returned_by','operation_date'],
   users: ['username','password','role','extra'],
   retired: ['id','name','serial_number','model','type','verification_date','valid_until','document_url','condition','taken_by','taken_where','taken_extra','taken_date','retired_date']
